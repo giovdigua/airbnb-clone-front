b8ebe912446bc5c922dbff5501719a1697451153
Giovanni DI GUARDO <g.diguardo@cineca.it>
2024-06-14 08:27:46 +0200

    Add Navbar and Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {Button} from "primeng/button";
 import {FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome";
 import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+import {NavbarComponent} from "./layout/navbar/navbar.component";
+import {FooterComponent} from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Button, FaIconComponent],
+  imports: [RouterOutlet, Button, FaIconComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
